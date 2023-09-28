7db2e9da7400590b10d5af9eb46390e550a50d15
Angel Romero <mrangelrcd@gmail.com>
2023-09-28 18:04:47 +0200

    Add tests for Home

---
+++ tests/Home.test.tsx
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/react";
+import { expect } from "vitest";
+import { it } from "vitest";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import Home from "../src/routes/Home";
+
+function setup() {
+  const router = createMemoryRouter(
+    [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/shop",
+        element: <h2>Shop</h2>,
+      },
+    ],
+    {
+      initialEntries: ["/"],
+    }
+  );
+
+  const user = userEvent.setup();
+  render(<RouterProvider router={router} />);
+  return user;
+}
+
+it("Home display lema text", () => {
+  setup();
+  expect(screen.getByText("This store is really cool!")).toBeInTheDocument();
+});
+
+it("Action button redirects you to /shop", async () => {
+  const user = setup();
+  await user.click(
+    screen.getByRole("button", {
+      name: /see products/i,
+    })
+  );
+
+  expect(
+    screen.getByRole("heading", {
+      name: "Shop",
+    })
+  ).toBeInTheDocument();
+});
